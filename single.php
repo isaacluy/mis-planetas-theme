be2e85ad010067b60520f01f97ac7a801cf40272
Isaac Luy <isaacluy@github.com>
2021-10-22 10:48:38 -0400

    Create single.php starter

---
+++ single.php
@@ -0,0 +1,17 @@
+<?php
+	/**
+	 * Home Page Template
+	 *
+	 * @package MisPlanetasTheme
+	 * @since 1.0.0
+	 */
+
+	// Basically includes header.php.
+	get_header();
+?>
+
+<h1> This is where the post content goes!</p>
+
+<?php
+	// Basically includes footer.php.
+	get_footer();
