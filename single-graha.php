dc8e18341687a68f026437a798e9b0d03c7a51f0
Isaac Luy <isaacluy@github.com>
2021-10-24 13:33:57 -0400

    Add grahas template v1.0.0

--- single-graha.php
+++ single-graha.php
@@ -8,9 +8,61 @@
 
 	// Basically includes header.php.
 	get_header();
-?>
+	get_template_part(
+		'template_parts/grahas',
+		'submenu'
+	);
+	?>
 
-<h1> This is where the GRAHA content goes!</p>
+<?php
+if ( have_posts() ) {
+	while ( have_posts() ) {
+		the_post();
+		$current_post_id           = get_the_ID();
+		$current_post_title        = get_the_title( $current_post_id );
+		$current_post_nombre_comun = esc_attr( get_post_meta( $current_post_id, 'nombreComun', true ) );
+
+		?>
+			<!-- ENTRY HEADER -->
+			<section class="bg-white rounded-xl p-8 mb-4 shadow lg:flex lg:p-0">
+			<?php
+			if ( has_post_thumbnail() ) {
+				?>
+				<img
+					class="
+						w-32
+						h-32
+						rounded-full
+						mx-auto
+						lg:mx-0 lg:h-auto lg:rounded-xl lg:rounded-r-none
+						shadow
+					"
+					src="<?php echo esc_attr( wp_get_attachment_url( get_post_thumbnail_id( get_the_ID() ) ) ); ?>"
+					alt="<?php echo esc_attr( $current_post_title ); ?> | <?php echo esc_attr( $current_post_nombre_comun ); ?>"
+				/>
+				<?php
+			}
+			?>
+				<div class="mt-2 text-center lg:text-left lg:m-4">
+					<h1
+						class="text-red-900 text-2xl text-center lg:text-left lg:text-3xl"
+					>
+						<?php echo esc_attr( $current_post_title ); ?>
+						<span class="text-sm italic">( <?php echo esc_attr( $current_post_nombre_comun ); ?> )</span>
+					</h1>
+					<p class="mt-1 text-base text-gray-600 italic lg:text-lg">
+						<?php the_excerpt(); ?>
+					</p>
+				</div>
+			</section>
+			<!-- END ENTRY HEADER -->
+
+			<!-- FAVORABLE VS DESFAVORABLE -->
+				<?php the_content(); ?>
+		<?php
+	}
+}
+?>
 
 <?php
 	// Basically includes footer.php.
---
+++ template_parts/grahas-submenu.php
@@ -0,0 +1,48 @@
+<?php
+	/**
+	 * Grahas Submenu
+	 *
+	 * @package MisPlanetasTheme
+	 * @since 1.0.0
+	 */
+
+?>
+			<!-- SUBMENU -->
+			<nav id="submenu" class="mb-4 text-red-700 underline">
+				<ol class="flex mb-4 space-x-4">
+					<li class="hover:text-red-500">
+						<a href="#detalles">Detalles</a>
+					</li>
+					<li class="hover:text-red-500"><a href="#favorable">Favorable</a></li>
+					<li class="hover:text-red-500">
+						<a href="#desfavorable">Desfavorable</a>
+					</li>
+					<li class="hidden hover:text-red-500 lg:block">
+						<a href="#fisico-salud">Físico y Salud</a>
+					</li>
+					<li class="hidden hover:text-red-500 lg:block">
+						<a href="#psicologia">Psicología</a>
+					</li>
+					<li class="hidden hover:text-red-500 lg:block">
+						<a href="#otros">Otros</a>
+					</li>
+				</ol>
+				<ol class="flex mb-4 space-x-4 lg:hidden">
+					<li class="hover:text-red-500">
+						<a href="#fisico-salud">Físico y Salud</a>
+					</li>
+					<li class="hover:text-red-500">
+						<a href="#psicologia">Psicología</a>
+					</li>
+					<li class="hover:text-red-500"><a href="#otros">Otros</a></li>
+				</ol>
+				<ol class="flex mb-4 space-x-4">
+					<li class="hover:text-red-500">
+						<a href="#karakas">Karakas Principales</a>
+					</li>
+					<li class="hover:text-red-500">
+						<a href="#karakas-fisicos-mentales">Karakas Físicos y Mentales </a>
+					</li>
+				</ol>
+			</nav>
+			<!-- END SUBMENU -->
