092cfee308d6c713efce8127667e934232a13f3f
Isaac Luy <isaacluy@github.com>
2021-10-29 12:10:07 -0400

    Minor header cleanup

--- header.php
+++ header.php
@@ -10,22 +10,12 @@
 <!DOCTYPE html>
 <html lang="es">
 	<head>
-	<!-- <title>Mis Planetas Wireframes</title> -->
-
 	<meta charset="UTF-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 	<?php
-		/**
-		 * Header
-		 *
-		 * @package MisPlanetasTheme
-		 * @since 1.0.0
-		 */
-
 		wp_head();
-
 	?>
 	</head>
 	<body class="bg-gray-50">
