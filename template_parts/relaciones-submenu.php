ac987b13df3893215e8be8f5487d96f00a984f7c
Isaac Luy <isaacluy@github.com>
2021-11-18 14:15:39 -0500

    Update submenu for relaciones

--- template_parts/relaciones-submenu.php
+++ template_parts/relaciones-submenu.php
@@ -9,21 +9,150 @@
 ?>
 <!-- SUBMENU -->
 		<nav id="submenu" class="mb-4 text-red-700 underline">
-			<ol class="flex mb-4 space-x-4">
-				<li class="hover:text-red-500">
-				<a href="#gabinete">Posición en el Gabinete</a>
-				</li>
-				<li class="hover:text-red-500">
-				<a href="#amistades">Amistades entre Grahas</a>
-				</li>
-				<li class="hidden hover:text-red-500 lg:block">
-				<a href="#puntos-cardinales">Regencia de Puntos Cardinales</a>
-				</li>
-			</ol>
-			<ol class="flex mb-4 space-x-4 lg:hidden">
-				<li class="hover:text-red-500">
-				<a href="#puntos-cardinales">Regencia de Puntos Cardinales</a>
-				</li>
-			</ol>
-			</nav>
+			<div id="linea-1">
+				<ol class="flex mb-4 space-x-4">
+					<li class="hover:text-red-500">
+					<a href="#frecuencias">Frecuencias</a>
+					</li>
+					<li class="hover:text-red-500">
+					<a href="#amistades-permanentes">Amistades Permanentes</a>
+					</li>
+					<li class="hidden hover:text-red-500 lg:block">
+					<a href="#temperamento">Temperamento</a>
+					</li>
+					<li class="hidden hover:text-red-500 lg:block">
+					<a href="#naturaleza-ensenanza">Naturaleza de la enseñanza</a>
+					</li>
+					<li class="hidden hover:text-red-500 lg:block">
+					<a href="#amistades-especificas">Amistades Permanentes Específicas</a>
+					</li>
+				</ol>
+				<ol class="flex mb-4 space-x-4 lg:hidden">
+					<li class="hover:text-red-500">
+					<a href="#temperamento">Temperamento</a>
+					</li>
+					<li class="hover:text-red-500">
+					<a href="#naturaleza-ensenanza">Naturaleza de la enseñanza</a>
+					</li>
+				</ol>
+				<ol class="flex mb-4 space-x-4 lg:hidden">
+					<li class="hover:text-red-500">
+					<a href="#amistades-especificas">Amistades Permanentes Específicas</a>
+					</li>
+				</ol>
+			</div>
+			<div id="linea-2">
+				<ol class="flex mb-4 space-x-4">
+					<li class="hover:text-red-500">
+					<a href="#graha-graha">Combinaciones Graha-Graha</a>
+					</li>
+					<li class="hidden hover:text-red-500 lg:block">
+					<a href="#constelaciones">Constelaciones</a>
+					</li>
+					<li class="hidden hover:text-red-500 lg:block">
+					<a href="#aspectos">Aspectos</a>
+					</li>
+					<li class="hidden hover:text-red-500 lg:block">
+					<a href="#graha-rishi">Relaciones Graha-Rishi</a>
+					</li>
+					<li class="hidden hover:text-red-500 lg:block">
+					<a href="#casas">Bhava Nitya Karaka</a>
+					</li>
+				</ol>
+				<ol class="flex mb-4 space-x-4 lg:hidden">
+					<li class="hover:text-red-500">
+					<a href="#constelaciones">Constelaciones</a>
+					</li>
+					<li class="hover:text-red-500">
+					<a href="#aspectos">Aspectos</a>
+					</li>
+				</ol>
+				<ol class="flex mb-4 space-x-4 lg:hidden">
+					<li class="hover:text-red-500">
+					<a href="#graha-rishi">Relaciones Graha-Rishi</a>
+					</li>
+				</ol>
+				<ol class="flex mb-4 space-x-4 lg:hidden">
+					<li class="hover:text-red-500">
+					<a href="#casas">Bhava Nitya Karaka</a>
+					</li>
+				</ol>
+			</div>
+			<div id="linea-3">
+				<ol class="flex mb-4 space-x-4">
+					<li class="hover:text-red-500">
+					<a href="#bhava-bhava">Relaciones entre Bhavas</a>
+					</li>
+					<li class="hidden hover:text-red-500 lg:block">
+					<a href="#combustion-solar">Combustión Solar</a>
+					</li>
+					<li class="hidden hover:text-red-500 lg:block">
+					<a href="#gabinete-varnas">Gabinete y Varnas</a>
+					</li>
+					<li class="hidden hover:text-red-500 lg:block">
+					<a href="#graha-rishi">Relaciones Graha-Rishi</a>
+					</li>
+					<li class="hidden hover:text-red-500 lg:block">
+					<a href="#edades">Edades</a>
+					</li>
+					<li class="hidden hover:text-red-500 lg:block">
+					<a href="#mb-mg-dhatu-indriyas">MB, MG, Dhatu e Indriyas</a>
+					</li>
+				</ol>
+				<ol class="flex mb-4 space-x-4 lg:hidden">
+					<li class="hover:text-red-500">
+					<a href="#combustion-solar">Combustión Solar</a>
+					</li>
+					<li class="hover:text-red-500">
+					<a href="#gabinete-varnas">Gabinete y Varnas</a>
+					</li>
+				</ol>
+				<ol class="flex mb-4 space-x-4 lg:hidden">
+					<li class="hover:text-red-500">
+					<a href="#edades">Edades</a>
+					</li>
+					<li class="hover:text-red-500">
+					<a href="#mb-mg-dhatu-indriyas">MB, MG, Dhatu e Indriyas</a>
+					</li>
+				</ol>
+			</div>
+			<div id="linea-4">
+				<ol class="flex mb-4 space-x-4">
+					<li class="hover:text-red-500">
+					<a href="#kalapurusha">Kalapurusha</a>
+					</li>
+					<li class="hover:text-red-500">
+					<a href="#puntos-cardinales">Puntos Cardinales</a>
+					</li>
+					<li class="hidden hover:text-red-500 lg:block">
+					<a href="#tipo-planeta">Tipo de Planeta</a>
+					</li>
+					<li class="hidden hover:text-red-500 lg:block">
+					<a href="#movimiento">Movimiento Promedio</a>
+					</li>
+					<li class="hidden hover:text-red-500 lg:block">
+					<a href="#flores-granos-arboles">Flores, Granos/Cereales, Árbol</a>
+					</li>
+					<li class="hidden hover:text-red-500 lg:block">
+					<a href="reinos">Reinos</a>
+					</li>
+				</ol>
+				<ol class="flex mb-4 space-x-4 lg:hidden">
+					<li class="hover:text-red-500">
+					<a href="#tipo-planeta">Tipo de Planeta</a>
+					</li>
+					<li class="hover:text-red-500">
+					<a href="#movimiento">Movimiento Promedio</a>
+					</li>
+				</ol>
+				<ol class="flex mb-4 space-x-4 lg:hidden">
+					<li class="hover:text-red-500">
+					<a href="#flores-granos-arboles">Flores, Granos, Árbol</a>
+					</li>
+					<li class="hover:text-red-500">
+					<a href="reinos">Reinos</a>
+					</li>
+				</ol>
+			</div>
+		</nav>
 		<!-- END SUBMENU -->
